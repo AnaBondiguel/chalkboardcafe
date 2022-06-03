eb5c399fdc20a4cb58a668fea17b312b8bf8dd30
AnaBondiguel <michaelyangyang@hotmail.com>
2022-06-04 01:31:25 +1000

    fined routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :menus
   devise_for :users
   root 'home#page'
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
